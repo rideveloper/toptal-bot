dbf776e1facc910bf33a14536ff66582cb940c35
rideveloper <38904941+rideveloper@users.noreply.github.com>
2021-10-04 17:08:16 +0100

    Add files via upload

---
+++ seleniumbot/src/main/java/com/example/seleniumbot/SeleniumbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.seleniumbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumbotApplication.class, args);
+	}
+
+}
---
+++ seleniumbot/src/test/java/com/example/seleniumbot/SeleniumbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seleniumbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
